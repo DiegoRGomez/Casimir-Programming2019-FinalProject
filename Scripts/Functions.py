567e8d2e8287b36ccb3120b14699b854e64daf04
Diego R. Gomez <rga.diego@gmail.com>
2019-11-07 19:29:05 +0100

    Starting working on function for opening .nd2 files

--- Scripts/Functions.py
+++ Scripts/Functions.py
@@ -26,11 +26,35 @@ def GetIterables(FileList):
     StemList = [element[:Idx_EndOfStem] for element in FileList] #Different <stem> strings correspond to different FOVs, get a list of stems
     StemList = list(set(StemList)) #(remove repeated entires) #Contains the names of the different fields of view (FOVs)
     #NumberOfFOVs = len(StemList)
-    return NumberOfChannels, StemList
+    return NumberOfChannels, StemList#, NumberOfFOVs
 
+def LoadImagesInFOV(Path, FileList, NumberOfChannels, stem):
+    FilesToRead = list(FileList) #If we do only FilesToRead = FileList, then modifying FilesToRead also modifies FileList --> wtf?! python is stupid sometimes. The list() command helps to tell python that the two lists should be allocated in independent memory slots
+    for FileName in FileList: #If we iterate through the elements of FilesToRead, then something goes wrong. Apparently you cannot iterate through a list and modify it at the same time. Again, python be stupid
+        if not stem in FileName:
+            FilesToRead.remove(FileName)
+    StackCollection = {} #Dictionary that stores the stacks corresponding to different channels
+    for FileName in FilesToRead:
+        with ND2Reader(Path + FileName) as stack:
+            
+            StackCollection[stack.metadata['channels'][0]] = stack
 
-L = ScanFolder("../Images/Hy69/")
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 20))
+    ax1.imshow(SC["BF"][0])
+    ax2.imshow(SC["GFP"][0])
+    return StackCollection
+    
+
+Path = "../Images/Hy69/"
+L = ScanFolder(Path)
 print(L)
-Nch, sList = GetIterables(L)
-print(Nch)
-print(sList)
+#nCh, sList, nFOVs = GetIterables(L)
+nCh, sList = GetIterables(L)
+#print(Nch)
+#print(sList)
+
+SC = LoadImagesInFOV(Path, L, nCh, "Point0006")
+
+#fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 20))
+#ax1.imshow(SC["BF"])
+#ax2.imshow(SC["GFP"])
