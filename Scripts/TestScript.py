18be086e6148fadd9fc437e069b028d987cfde44
Diego R. Gomez <rga.diego@gmail.com>
2019-11-07 19:26:32 +0100

    Added tests for accessing and storing content of .nd2 files

--- Scripts/TestScript.py
+++ Scripts/TestScript.py
@@ -12,14 +12,26 @@ fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 20))
 
 
 with ND2Reader('../Images/Hy69/Point0005_Seq0010.nd2') as images:
-  ax1.imshow(images[0])
+    copied_images = [ im.copy() for im in images ]
+    ax1.imshow(copied_images[1])
 
+#fig, ax3 = plt.subplots()
+ax2.imshow(copied_images[1])
+
+
+#J = ND2Reader('../Images/Hy69/Point0005_Seq0010.nd2')
+#ax2.imshow(J[0])
   
-with ND2Reader('../Images/Hy69/Point0005_Seq0010.nd2') as images:  
-    ax2.imshow(images[0])
-    print('%d x %d px' % (images.metadata['width'], images.metadata['height']))
-    print(images.metadata)
-    for el in images.metadata:
-        print(el, "-->", images.metadata[el])
-    print(images.sizes)
- 
+
+
+#with ND2Reader('../Images/Hy69/Point0005_Seq0011.nd2') as images:  
+#    ax2.imshow(images[0])
+#    print('%d x %d px' % (images.metadata['width'], images.metadata['height']))
+#    print(images.metadata)
+#    print("chan:", images.metadata['channels'][0])
+#    for el in images.metadata:
+#        print(el, "-->", images.metadata[el])
+#    print(images.sizes)
+# 
+#fig, ax3 = plt.subplots()
+#ax3.imshow(images[0])
